b4baae0a5b284302c269e4569586a6c486664ffa
Mihai Lazari <l3m3a3@gmail.com>
2024-12-22 11:02:53 +0200

    use loose: false for babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+const loose = false;
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', {loose}],
+    ['@babel/plugin-transform-private-methods', {loose}],
+    ['@babel/plugin-transform-private-property-in-object', {loose}],
+  ],
 };
